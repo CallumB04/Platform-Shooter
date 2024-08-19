edfd0638448d32fb64955646f3ecc60bb675f28d
Cal <callumburgoyne04@gmail.com>
2024-08-19 14:28:07 +0100

    Including SFML libraries and other libraries needed for state machine

---
+++ src/includes.h
@@ -0,0 +1,10 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+#include <iostream>
+#include <memory>
+#include <stack>
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
